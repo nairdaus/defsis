27b6ac6ddea4179d24b7871eb11d4b977870c5b0
nairdaus <mansilladrian@gmail.com>
2015-12-16 15:55:23 -0400

    modulos usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,10 +6,36 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .forms import RegistroUserForm
 
 
 def create_user_view(request):
-	return render()
+	if request.method == 'POST':
+		form = RegistroUserForm(request.POST)
+		if form.is_valid():
+			cleaned_data = form.cleaned_data
+			nombres = cleaned_data.get('nombres')
+			apellidos = cleaned_data.get('apellidos')
+			username = cleaned_data.get('usuario')
+			password = cleaned_data.get('password')
+			tipo = cleaned_data.get('tipo')
+
+			user_model = User.objects.create_user(username = username,  password = password)
+			user_model.first_name = nombres
+			user_model.last_name = apellidos
+			user_model.acces = tipo
+
+			user_model.save()
+			print 'Usuario creado correctamente'
+			
+			return HttpResponseRedirect('/usuarios/')
+
+	else:
+		form = RegistroUserForm()
+	context = {
+		'form': form
+	}
+	return render(request, 'usuarios/nuevo_usuario.html', context)
 
 def list_user_view(request):
 	usuarios = User.objects.all()
@@ -50,6 +76,30 @@ def edit_user_view(request, id_user):
 	usuario = User.objects.get(id = id_user)
 	template = 'usuarios/editar_usuario.html'
 	return render(request, template, {'usuario': usuario} )
+
+@csrf_exempt
+@login_required(login_url = '/')
+def save_modify(request):
+	user_id = request.POST['id']
+	nombre = request.POST['first_name']
+	apellidos = request.POST['last_name']
+	usuario = request.POST['user_name']
+	clave = request.POST['password']
+	seleccion = request.POST['seleccion']
+	tipos = {'Psicologo(a)':1,'Trabajador(a) Social':2,'Abogado(a)':3,'Jefatura':4,'Secretaria':5}
+	tipo_usuario = tipos[seleccion]
+
+	current_user = User.objects.get(id=user_id)
+	current_user.first_name = nombre
+	current_user.last_name = apellidos
+	current_user.username = usuario
+	current_user.password = clave
+	current_user.acces = tipo_usuario
+	current_user.save()
+
+	print 'Usuario Actualizado correctamente'
+
+	return HttpResponseRedirect('/usuarios/')
 
 # Esta vista es de prueba, y debe ser trasladada al modulo "denuncias"
 @login_required(login_url = '/')
