dcbae6e469e156403a64155b1060abd6b4b7038e
mcli <mclimber28@gmail.com>
2015-12-21 20:26:34 -0400

    otro del calendario

--- usuarios/views.py
+++ usuarios/views.py
@@ -69,7 +69,13 @@ def registrar_actuaciones(request):
 def listado_casos(request):
 	template = 'listadoCasos.html'
 	return render(request, template)
+
 @login_required(login_url = '/')
 def detalles_del_caso(request):
 	template = 'detallesDelCaso.html'
+	return render(request, template)
+
+@login_required(login_url = '/')
+def calendario(request):
+	template = 'calendario.html'
 	return render(request, template)
