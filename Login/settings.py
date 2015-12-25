76c5fc0a97ff92f2e4ee90177733bc233b7102b8
mcli <mclimber28@gmail.com>
2015-12-24 21:56:11 -0400

    editando estatics

--- Login/settings.py
+++ Login/settings.py
@@ -72,7 +72,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../Login/static'),
+    os.path.join(BASE_DIR, '/static/'),
 )
 
 TEMPLATES = [
