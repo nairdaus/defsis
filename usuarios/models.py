a845511ae501ea9aa97b29f3a6f18a5a9e6351da
mcli <mclimber28@gmail.com>
2015-12-29 06:49:59 -0400

    postgresss dia 7

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,7 +3,4 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-User.add_to_class('access', models.IntegerField(null = True, blank = True))
-
-
 # Create your models here.
