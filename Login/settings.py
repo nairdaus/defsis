431a10effb68149d74bbb5c75fa87dd63fe2d47b
mcli28 <mclimber28@gmail.com>
2015-12-30 11:07:13 -0400

    postgress dia 25

--- Login/settings.py
+++ Login/settings.py
@@ -88,6 +88,14 @@ DATABASES = {
 		'HOST': 'ec2-54-83-204-228.compute-1.amazonaws.com', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
 		'PORT': '5432',                      # Set to empty string for default.
 	}
+	# 'default': {
+		# 'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+		# 'NAME': 'defensorialocal',                      # Or path to database file if using sqlite3.
+		# 'USER': 'mcli28',
+		# 'PASSWORD': 'mcli28',
+		# 'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+		# 'PORT': '5432',                      # Set to empty string for default.
+	# }
 }
 DATABASES['default'] =  dj_database_url.config()
 
