03dc0ec374f2387553ffd120731612422dd24f34
mcli28 <mclimber28@gmail.com>
2015-12-30 09:34:47 -0400

    postgress dia 22

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,5 +4,5 @@ from django.db import models
 from django.contrib.auth.models import User
 from denuncias.models import Defensoria
 
-User.add_to_class('acces', models.IntegerField(null = True, blank = True))
-User.add_to_class('defensoria', models.ForeignKey(Defensoria, blank = True, null = True))
+#User.add_to_class('acces', models.IntegerField(null = True, blank = True))
+#User.add_to_class('defensoria', models.ForeignKey(Defensoria, blank = True, null = True))
